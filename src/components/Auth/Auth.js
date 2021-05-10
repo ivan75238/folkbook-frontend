eedd34ea5d9bb8318d5e1a86e8604cdb303b7de4
Фонтош Иван <fontosh.ivan@gmail.com>
2021-05-10 12:21:07 +0800

    - Авторизация/Регистрация через ВК

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -73,6 +73,12 @@ const LogoWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    cursor: pointer;
+    
+    svg {
+        width: 100%;
+        height: 100%;
+    }
 `;
 
 const OAuthWrapper = styled.div`
