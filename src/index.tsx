7416dddde6097e196a415f05278c3e6d71f48105
Фонтош Иван <fontosh.ivan@gmail.com>
2021-05-27 09:19:14 +0800

    - Переработка на TypeScript закончена

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
-import {store} from 'store';
+import {store} from './store';
 import './common/fonts/fonts.scss';
 
 import {ThemeProvider} from "styled-components";
-import {theme} from "config/theme";
-import {App} from "components/App";
+import {theme} from "./config/theme";
+import {App} from "./components/App";
 
 ReactDOM.render(
     <Provider store={store}>
